11256909f89b803792964c5b3ee0bb4fb69b6a58
CodeWithEyeasin <mreyeasinarafat@gmail.com>
2024-06-24 18:26:45 +0600

    new

--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,8 +1,9 @@
+import 'package:ecommerce_app/presentation/screens/email_verification_screen.dart';
 import 'package:ecommerce_app/presentation/screens/home_screen.dart';
-import 'package:ecommerce_app/presentation/utility/assets_path.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/route_manager.dart';
+
+import '../widgets/app_logo.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -15,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   Future<void> _moveToNextScreen() async {
    await Future.delayed(const Duration(seconds: 2),);
-   Get.to(()=>HomeScreen());
+   Get.to(()=>const EmailVerificationScreen());
   }
 
   @override
@@ -27,20 +28,22 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Column(
           children: [
-            const Spacer(),
-            SvgPicture.asset(AssetsPath.appLogoSvg,width:100,),
-            const Spacer(),
-            const CircularProgressIndicator(),
-            const SizedBox(height: 16,),
-            const Text('Version 1.0'),
-            const SizedBox(height: 16,),
+            Spacer(),
+            AppLogo(),
+            Spacer(),
+            CircularProgressIndicator(),
+            SizedBox(height: 16,),
+            Text('Version 1.0'),
+            SizedBox(height: 16,),
           ],
         ),
       ),
     );
   }
 }
+
+
